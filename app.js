5acded3f7ce42b558d1cf74799c21bfd94521e62
Darya Shlyakhovaya <100313025+DariSh92@users.noreply.github.com>
2023-11-18 22:28:18 +0200

    init

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 
 // app.use("/api/contacts", contactsRouter);
