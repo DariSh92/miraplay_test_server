c3979f7a35e7ea22eb0488a787198717340824dc
Darya Shlyakhovaya <100313025+DariSh92@users.noreply.github.com>
2023-11-18 17:39:06 +0200

    init

--- server.js
+++ server.js
@@ -2,20 +2,31 @@ const mongoose = require("mongoose");
 const app = require("./app");
 mongoose.set("strictQuery", true);
 
-const { DB } = process.env;
+const { DB, PORT } = process.env;
 
 
 
 mongoose
-  .connect(DB, {
-    usenewurlparser: true,
-    useunifiedtopology: true,
-  })
-  .then(() => {
-    app.listen(3000);
-    console.log(`Server running. Use our API on port: 3000`);
-  })
+mongoose
+  .connect(DB)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Database connection successful. API is on port: ${PORT}`)
+    )
+  )
   .catch((error) => {
-    console.log(`can not connect to database, ${error}`);
+    console.log(error.message);
     process.exit(1);
   });
+  // .connect(DB, {
+  //   usenewurlparser: true,
+  //   useunifiedtopology: true,
+  // })
+  // .then(() => {
+  //   app.listen(3000);
+  //   console.log(`Server running. Use our API on port: 3000`);
+  // })
+  // .catch((error) => {
+  //   console.log(`can not connect to database, ${error}`);
+  //   process.exit(1);
+  // });
